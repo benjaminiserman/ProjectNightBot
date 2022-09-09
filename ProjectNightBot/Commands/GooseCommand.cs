774cd76a84b9a1e06221082ab13b67f346099db7
Benjamin E. Iserman <winggar1228@gmail.com>
2022-09-09 11:10:52 -0500

    minor formatting fixes

--- ProjectNightBot/Commands/GooseCommand.cs
+++ ProjectNightBot/Commands/GooseCommand.cs
@@ -18,8 +18,8 @@ internal class GooseCommand : ICommand
 
 	public async Task Execute(SocketSlashCommand command)
 	{
-        var path = @$"Images/goose{Random.Shared.Next(1,6)}.jpg";
-        var fs = File.OpenRead(path);
+		var path = @$"Images/goose{Random.Shared.Next(1,6)}.jpg";
+		var fs = File.OpenRead(path);
 		await command.RespondWithFileAsync(fs, path);
 	}
 }
