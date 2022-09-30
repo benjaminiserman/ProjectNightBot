473fe7afacfbadcb9a98db1c5130c3c1d30bcf68
GageSch <gageschacher@gmail.com>
2022-09-29 22:19:42 -0500

    Fixed files name

--- ProjectNightBot/Commands/GooseCommand.cs
+++ ProjectNightBot/Commands/GooseCommand.cs
@@ -8,11 +8,11 @@ internal class GooseCommand : ICommand
 {
 	public string Name => "goose";
 
-	public string[] Files;
+	private string[] files;
 
 	public SlashCommandProperties Build()
 	{
-		Files = Directory.GetFiles("Images/Geese");
+		files = Directory.GetFiles("Images/Geese");
 
 		return new SlashCommandBuilder()
 			.WithName(Name)
@@ -22,8 +22,8 @@ internal class GooseCommand : ICommand
 
 	public async Task Execute(SocketSlashCommand command, DiscordSocketClient client)
 	{
-		var i = Random.Shared.Next(0, Files.Length);
-		var path = Files[i];
+		var i = Random.Shared.Next(0, files.Length);
+		var path = files[i];
 		var fs = File.OpenRead(path);
 		await command.RespondWithFileAsync(fs, path);
 	}
